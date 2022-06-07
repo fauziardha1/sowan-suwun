2b417d492c0d239aa09519891556a2645292854e
fauziardha1 <fauziardasaputra@gmail.com>
2022-06-07 22:35:45 +0700

    add detail page but not done yet

--- javascript/interaction.js
+++ javascript/interaction.js
@@ -1,12 +1,21 @@
-mobiscroll.card('.demo-card li', {
-    theme: 'ios',
-    themeVariant: 'light'
-});
+// mobiscroll.card('.demo-card li', {
+//     theme: 'ios',
+//     themeVariant: 'light'
+// });
 
-mobiscroll.scrollview('.demo-card', {
-    theme: 'ios',
-    themeVariant: 'light',
-    layout: 'fixed',
-    itemWidth: 134,
-    snap: false
-});
+// mobiscroll.scrollview('.demo-card', {
+//     theme: 'ios',
+//     themeVariant: 'light',
+//     layout: 'fixed',
+//     itemWidth: 134,
+//     snap: false
+// });
+
+var cards = document.getElementsByClassName('card');
+for (let index = 0; index < cards.length; index++) {
+    const element = cards[index];
+    element.addEventListener('click', function () {
+        window.location.href = "./detail_page.html";
+    });
+
+}
